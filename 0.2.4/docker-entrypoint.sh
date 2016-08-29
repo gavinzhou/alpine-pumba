cc9a86c3c145e24ad9aac621e50f58ff16993d2f
gavinzhou <gavin.zhou@gmail.com>
2016-08-29 14:45:00 +0000

    update version to 0.2.4

--- 0.2.4/docker-entrypoint.sh
+++ 0.2.4/docker-entrypoint.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/sbin/dumb-init /bin/sh
 set -e
 
 if [ -S /var/run/docker.sock ]; then
   chown -R pumba:pumba /var/run/docker.sock
 fi
 
-exec dumb-init gosu pumba:pumba "$@"
+exec gosu pumba:pumba "$@"
 
